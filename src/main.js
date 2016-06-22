07a7ad54deb310eef7cbf4533aa8e6b9fb559dc6
Nathan Bourke <nathanrbourke@gmail.com>
2016-06-22 19:52:09 -0400

    component

--- src/main.js
+++ src/main.js
@@ -2,13 +2,29 @@ import './main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Track = props => {
+  if(props.playing) {
+    var playIcon = <i className="fa fa-pause-circle-o" />;
+  } else {
+    var playIcon = <i className="fa fa-play-circle-o" />;
+  }
+
+  return(
+    <li className="track">
+      {playIcon}
+      <span className="title">{props.title}</span>
+      <span className="duration">{props.duration}</span>
+    </li>
+  );
+}
+
 let trackOne = <li className="track">
   <i className="fa fa-play-circle-o" />
   <span className="title">Talking Heads - Found A Job</span>
   <span className="duration">4:30</span>
 </li>;
 
-let trackTwo = <li className="track selected">
+let trackTwo = <li className="track">
   <i className="fa fa-play-circle-o" />
   <span className="title">Phoenix - Entertainment</span>
   <span className="duration">3:40</span>
